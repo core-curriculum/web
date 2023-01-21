f713af66ec2e06b7a085fb7c64f7a3a0e2f7a516
Takeshi Kondo <ncukondo@gmail.com>
2023-01-21 07:04:15 +0000

    fix navibar style

--- src/components/LinkToItemList.tsx
+++ src/components/LinkToItemList.tsx
@@ -6,11 +6,17 @@ type Prop = {
 };
 const LinkToItemList = ({ count, href }: Prop) => {
   return (
-    <Link {...{ href }} className="flex items-center justify-center gap-x-1">
-      <span className={`${count > 0 ? "text-sky-300" : "text-gray-400"}`}>リスト</span>
+    <Link
+      {...{ href }}
+      className="flex items-center justify-center gap-x-1 rounded-md p-2 hover:bg-sky-100"
+    >
+      <span className={`${count > 0 ? "text-sky-300" : "text-gray-400"} max-md:text-sm`}>
+        リスト
+      </span>
       <span
         className={
           "flex h-6 w-6 items-center justify-center rounded-full text-sm text-white " +
+          "max-md:h-5 max-md:w-5 max-md:text-xs " +
           `${count > 0 ? "bg-sky-300" : "bg-gray-400"}`
         }
       >
--- src/components/NaviBar.tsx
+++ src/components/NaviBar.tsx
@@ -30,19 +30,24 @@ const NaviBar = () => {
           <ToggleIcon />
         </label>
       </div>
-      <Image className="ml-2 max-lg:hidden" src="/logo.svg" width="50" height="50" alt="" />
-      <div className="flex-1 px-2 text-lg font-extrabold">モデルコアカリキュラム</div>
-      <Suspense fallback="">
-        <LinkToItemList href="./list" count={items.length} />
-      </Suspense>
-      <div className="mx-2 flex-none px-2">
-        <Link href="/search" passHref>
-          <MdSearch
-            title="検索"
-            className="rounded-md hover:bg-sky-100"
-            size="2rem"
-            color="rgb(125 211 252)"
-          />
+
+      <div className="flex flex-1 px-2 text-lg font-extrabold text-sky-400 max-md:text-base">
+        <Link
+          href="/"
+          className="flex flex-none items-center gap-2 rounded-md pr-2 hover:bg-sky-100"
+          passHref
+        >
+          <Image className="ml-2 max-lg:hidden" src="/logo.svg" width="50" height="50" alt="" />
+          モデルコアカリキュラム
+        </Link>
+      </div>
+      <div className="mr-2 flex flex-none items-center">
+        <Suspense fallback="">
+          <LinkToItemList href="./list" count={items.length} />
+        </Suspense>
+        <Link href="/search" className="flex items-center rounded-md p-2 hover:bg-sky-100" passHref>
+          <div className="mr-1 text-sm text-sky-400 max-lg:hidden">検索</div>
+          <MdSearch title="検索" className="" size="2rem" color="rgb(125 211 252)" />
         </Link>
       </div>
     </div>
