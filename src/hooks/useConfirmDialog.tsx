63eee700457b22b1dad585f45c7c19bf6ae526eb
Takeshi Kondo <ncukondo@gmail.com>
2023-02-25 12:12:43 +0000

    add confirm dialog when sharing

--- src/hooks/useConfirmDialog.tsx
+++ src/hooks/useConfirmDialog.tsx
@@ -1,32 +1,43 @@
-import { atom, useAtom } from "jotai";
+import { atom, useAtom, createStore, Provider } from "jotai";
 import { ConfirmDialog } from "@components/ConfirmDialog";
 import type { ConfirmDialogProps as Props } from "@components/ConfirmDialog";
 
+type ShowDialogProps = Omit<Props, "show" | "onClose">;
+
 const showAtom = atom(false);
 const propAtom = atom<Omit<Props, "show">>({ content: "", onClose: () => null });
+const store = createStore();
 
-const Dialog = () => {
+const InnerDialog = () => {
   const [show] = useAtom(showAtom);
   const [props] = useAtom(propAtom);
   return <ConfirmDialog {...{ ...props, show }} />;
 };
 
+const Dialog = () => {
+  return (
+    <Provider store={store}>
+      <InnerDialog />
+    </Provider>
+  );
+};
+
+const showDialog = (props: ShowDialogProps) => {
+  const emptyProp = { content: "", title: "", choises: [], onClose: () => null };
+  return new Promise<string>(resolve => {
+    const onClose = (key: string) => {
+      store.set(showAtom, false);
+      store.set(propAtom, emptyProp);
+      resolve(key);
+    };
+    store.set(showAtom, true);
+    store.set(propAtom, { ...props, onClose });
+  });
+};
+
 const useConfirmDialog = () => {
-  type PartialProps = Omit<Props, "show" | "onClose">;
-  const [, setShow] = useAtom(showAtom);
-  const [, setProps] = useAtom(propAtom);
-  const showDialog = (props: PartialProps) => {
-    return new Promise<string>((resolve) => {
-      const onClose = (key: string) => {
-        setShow(false);
-        console.log("close");
-        resolve(key);
-      };
-      setProps({ ...props, onClose });
-      setShow(true);
-    });
-  };
   return { showDialog };
 };
 
-export { useConfirmDialog, Dialog as ConfirmDialog };
+export { useConfirmDialog, Dialog as ConfirmDialog, showDialog };
+export type { ShowDialogProps };
