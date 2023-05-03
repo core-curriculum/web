71be35b3997e0a43eb4897ac79bdd99b0925690a
Takeshi Kondo <ncukondo@gmail.com>
2023-05-03 08:05:03 +0000

    update translation

--- src/services/i18n/text.ts
+++ src/services/i18n/text.ts
@@ -53,7 +53,7 @@ const text = {
       // eslint-disable-next-line max-len
       ja:`一般社団法人日本医学教育学会(医学教育学会)は、文部科学省「大学における医療人養成の在り方に関する調査研究委託事業」による委託に基づき「医学教育モデル・コア・カリキュラム（令和4年度改訂版）」を作成いたしました。このページは医学教育学会「モデル・コア・カリキュラム改訂等に関する調査研究チーム」が、「医学教育モデル・コア・カリキュラム（令和4年度改訂版）」に基づいて作成しています。`,
       // eslint-disable-next-line max-len
-      en:`This page contains the official English translation of the original Japanese revision, produced by the Model Core Curriculum Expert Research Committee, as part of a project commissioned by the Ministry of Education, Culture, Sports, Science and Technology (MEXT).`
+      en:`This page contains the official English translation of the original Japanese revision, produced by the Model Core Curriculum Expert Research Committee of the Japan Society for Medical Education, as part of a project commissioned by the Ministry of Education, Culture, Sports, Science and Technology (MEXT).`
     },
     linkMextText: {
       ja: "文部科学省モデル・コア・カリキュラム公表ページ",
@@ -72,7 +72,9 @@ const text = {
   "@pages/index": {
     outcomesTitle: {ja:"資質・能力",en:"Outcomes"},
     discription: {
+      // eslint-disable-next-line max-len
       ja: "モデル・コア・カリキュラムでは、医学生が卒業するまでに身に付けるべき能力を10の資質・能力(第1層)として提示し、詳細を第2層から4層までの項目と、関連づけられた別表で示しています。",
+      // eslint-disable-next-line max-len
       en: "The Model Core Curriculum presents 10 outcomes (the first layer) that medical students should acquire by the time they graduate, and details are shown in items from the second to fourth layers and related tables."
     },
     title: {
